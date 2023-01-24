5d9d2e263b2201e2b3f0600aff16b4a5756aaaf7
SerhiiChynin <sergey.chinin@gmail.com>
2023-01-24 17:29:26 +0200

    homework

--- js/class.js
+++ js/class.js
@@ -14,3 +14,31 @@ let firstStudent = new Student(a, b, 'PapaJohn');
 console.log(firstStudent);
 console.log(firstStudent.getStudentCourses());
 console.log(firstStudent.validatePassword());
+
+
+
+// Home Work
+
+
+let w = 100 + "px";
+let h = 100 + "px";
+let back = 'orange';
+let v = 'hello baby'
+class Button {
+    constructor(width, height, background, value){
+        this.width = width;
+        this.height = height;
+        this.background = background;
+        this.value = value;
+    }
+    render() {           
+            let bb = document.createElement('button');
+            bb.innerHTML = 'Hello Baby'
+            document.querySelector('.out_1').appendChild(bb);
+        return btn.bb;
+    }
+}
+const btn = new Button(w,h,back,v)
+document.querySelector('.out_1').innerHTML = btn.value;
+
+        
